41af9264fb65eb195a38c29a9cc76294f364de3b
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-20 12:53:07 +0300

    Added functions for upvote and downvote buttons - values now show

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,7 +13,7 @@ export class QuoteComponent implements OnInit {
   }
   
   quotes: Quote[] = [
-    new Quote(0,"Hell hath no dury like a woman scorned","Whoever it was",new Date(2019,14,1),0,0,0),
+    new Quote(0,"Hell hath no fury like a woman scorned","Whoever it was",new Date(2019,14,1),0,0,0),
     new Quote(1,"I have a dream....","Martin Luther King Jnr",new Date(2019,18,5),0,0,0),
     new Quote(2,"Together we can","Obama",new Date(2009,2,4),0,0,0)
   ];
@@ -23,8 +23,14 @@ export class QuoteComponent implements OnInit {
     this.quotes[index].showDetails = !this.quotes[index].showDetails 
   }
 
-
-
+  upvoteQuote(index: number){
+    this.quotes[index].upvote += 1;
+  }
+  downvoteQuote(index: number){
+    this.quotes[index].downvote += 1;
+  }
   
+
+
 
 }
