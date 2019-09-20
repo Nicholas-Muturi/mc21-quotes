a6cd0c076df8b624fdf4ba0fdb9e55da1c030340
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-20 12:54:04 +0300

    Added delete function for delete button | Shortened long quotes when confirm alerts pop up

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -29,7 +29,21 @@ export class QuoteComponent implements OnInit {
   downvoteQuote(index: number){
     this.quotes[index].downvote += 1;
   }
-  
+  deleteQuote(index: number){
+    if(this.quotes[index].quote.length > 15){
+      let shortenedQuote = (this.quotes[index].quote.substr(0,15).concat("..."));
+
+      var confirmed = confirm(`Are you sure you want to delete the following quote: \"${shortenedQuote}\"`)
+    }
+    else {
+      var confirmed = confirm(`Are you sure you want to delete the following quote: \"${this.quotes[index].quote}\"`)
+    }
+
+    if(confirmed){
+      this.quotes.splice(index,1);
+    }
+    
+  }
 
 
 
