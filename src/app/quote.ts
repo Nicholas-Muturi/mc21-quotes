1ebe9d14dea098b27377bd5ecdb6ab465263a903
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-20 11:11:54 +0300

    Defined quote structure in class | created array of quotes to display

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+    showDetails: boolean;
+    
+    constructor(
+        public id: number,
+        public quote: string,
+        public author: string,
+        public datePassed: Date,
+        public upvote: number,
+        public downvote: number,
+        public voteDifference: number
+    ){
+        this.showDetails = false;
+    }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes: Quote[] = [
+    new Quote(0,"Hell hath no dury like a woman scorned","Whoever it was",new Date(2019,14,1),0,0,0),
+    new Quote(1,"I have a dream....","Martin Luther King Jnr",new Date(2019,18,5),0,0,0),
+    new Quote(2,"Together we can","Obama",new Date(2009,2,4),0,0,0)
+  ];
   constructor() { }
 
   ngOnInit() {
