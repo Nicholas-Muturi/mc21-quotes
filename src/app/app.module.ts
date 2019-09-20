6a9d1c1264be794cb5d0787f6760fa26463f6526
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-20 11:02:07 +0300

    Generated custom pipe 'datePassed'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DatePassedPipe } from './date-passed.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    DatePassedPipe
   ],
   imports: [
     BrowserModule,
