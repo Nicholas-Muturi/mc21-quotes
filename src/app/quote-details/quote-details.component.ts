1da788b87f907639e2a64c4182f2fa04e5d1633c
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-20 11:27:12 +0300

    Linked quote component to app root html | Created html Skeleton

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-quote-details',
+  selector: 'quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
