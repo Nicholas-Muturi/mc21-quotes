c023f090ddf6908b7f53d9f32f7f80fbdf35bebb
Nicholas M <nicholasmuturi1@gmail.com>
2019-09-21 14:53:38 +0300

    Implented function to retrieve highest voted quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,13 +13,20 @@ export class QuoteComponent implements OnInit {
   }
   
   quotes: Quote[] = [
-    new Quote(0,"Act as if what you do makes a difference. It does.","William James",new Date(2019,8,11),0,0,0),
-    new Quote(1,"Success is not final, failure is not fatal: it is the courage to continue that counts.","Winston Churchill",new Date(2019,5,18),0,0,0),
-    new Quote(2,"You are never too old to set another goal or to dream a new dream.","C.S. Lewis",new Date(2009,4,20),0,0,0),
-    new Quote(3,"It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.","William James",new Date(2019,8,19),0,0,0)
+    new Quote(1,"Act as if what you do makes a difference. It does.","William James",new Date(2019,8,11),0,0,0),
+    new Quote(2,"Success is not final, failure is not fatal: it is the courage to continue that counts.","Winston Churchill",new Date(2019,5,18),0,0,0),
+    new Quote(3,"You are never too old to set another goal or to dream a new dream.","C.S. Lewis",new Date(2009,4,20),0,0,0),
+    new Quote(4,"It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.","William James",new Date(2019,8,19),0,0,0)
   ];
 
-  //Button Functions
+  highestVotedQuote = {
+    voteCount: 0,
+    qID: 0,
+    quote: "",
+    author: ""
+  }
+
+  //Button Click Functions
   toggleDetails(index: number){
     this.quotes[index].showDetails = !this.quotes[index].showDetails 
   }
@@ -40,9 +47,13 @@ export class QuoteComponent implements OnInit {
 
   upvoteQuote(index: number){
     this.quotes[index].upvote += 1;
+    this.quotes[index].voteDifference += 1;
+    this.getHighestVoted();
   }
   downvoteQuote(index: number){
     this.quotes[index].downvote += 1;
+    this.quotes[index].voteDifference -= 1;
+    this.getHighestVoted();
   }
   deleteQuote(index: number){
     if(this.quotes[index].quote.length > 15){
@@ -55,6 +66,25 @@ export class QuoteComponent implements OnInit {
 
     if(confirmed){
       this.quotes.splice(index,1);
+      this.getHighestVoted();
+    }
+  }
+
+  //Functions
+  getHighestVoted(){
+    // It might not be the best optimized approach but atleast it works ^^
+    var sortedArray = [];
+    for(let i:number = 0; i<this.quotes.length;i++){
+      sortedArray.push(this.quotes[i].voteDifference)
+    }
+    this.highestVotedQuote.voteCount = Math.max.apply(null,sortedArray)
+
+    for(let i:number = 0; i<this.quotes.length;i++){
+      if(this.highestVotedQuote.voteCount == this.quotes[i].voteDifference){
+        this.highestVotedQuote.qID = this.quotes[i].id;
+        this.highestVotedQuote.quote = this.quotes[i].quote;
+        this.highestVotedQuote.author = this.quotes[i].author;
+      }
     }
   }
 
